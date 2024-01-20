e4c5b55d221345d6f87a2984d8514665245bfa03
Ilham Rahmat Akbar <ilhamrhmtkbr>
2024-01-21 04:11:26 +0700

    Optimize Response Api

--- app/Helpers/ResponseApiHelper.php
+++ app/Helpers/ResponseApiHelper.php
@@ -6,8 +6,15 @@ use Illuminate\Support\Facades\Response;
 
 class ResponseApiHelper
 {
-    public static function customApiResponse($success, $data = null, $successMessage = null, $errorMessage = null, $statusCodeSuccess = 200, $statusCodeError = 422)
+    public static function customApiResponse($success, $data = null, $successMessage = 'Database : success.', $errorMessage = 'Database : failed.', $statusCodeSuccess = 200, $statusCodeError = 422)
     {
+        // jika $success bukan bolean
+        if (!is_bool($success)) {
+            // jika is_object($success) digunakan untuk cek create, update, dan delete
+            // $success > 0 (cek apakah update dan delete berhasil)
+            $success = is_object($success) || $success > 0;
+        }
+
         return Response::json([
             'success' => $success,
             'data' => $data,
